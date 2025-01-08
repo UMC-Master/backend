d258c52e95b825e78610ab2234a0b8d13fecbaf2
lee-haeseung <lhser123@cau.ac.kr>
2025-01-09 00:52:24 +0900

    feat: 공통 에러 class 생성

--- src/errors.ts
+++ src/errors.ts
@@ -1,11 +1,18 @@
-export class DuplicateUserEmailError extends Error {
-    errorCode = "U001";
-    reason: string;
-    data: string;
-  
-    constructor(reason: string, data: any) {
-      super(reason);
-      this.reason = reason;
-      this.data = data;
-    }
+export class CommomError extends Error {
+  errorCode: string;
+  reason: string;
+  data: unknown;
+
+  constructor(errorCode: string, reason: string, data: unknown) {
+    super(reason);
+    this.errorCode = errorCode;
+    this.reason = reason;
+    this.data = data;
   }
+}
+
+export class DuplicateUserEmailError extends CommomError {
+  constructor(reason: string, data: unknown) {
+    super('U001', reason, data);
+  }
+}
