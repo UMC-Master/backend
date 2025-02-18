f80529d12350c3f2b75a930aa59b8556ed30c517
lee-haeseung <lhser123@cau.ac.kr>
2025-02-19 00:01:23 +0900

    feat: 정책 조회시 좋아요, 북마크, 생성일 함께 반환

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,14 +1,10 @@
 declare namespace Express {
   export interface Response {
     success(response: object, message?: string, code?: string): this;
-    error(options: { errorCode?: string; reason?: string | null; data?: undefined }): this;
+    error(options: {
+      errorCode?: string;
+      reason?: string | null;
+      data?: undefined;
+    }): this;
   }
 }
-
-
-
-
-
-
-
-
